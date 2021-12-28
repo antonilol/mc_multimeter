68d7ebe499d06ffa92218689d8f6b203d749a2f6
Antoni Spaanderman <56turtle56@gmail.com>
2021-12-28 17:56:35 +0100

    minecraft 1.18.x

--- src/main/java/com/antonilol/mc_multimeter/mc/Main.java
+++ src/main/java/com/antonilol/mc_multimeter/mc/Main.java
@@ -39,7 +39,7 @@ public class Main implements ClientModInitializer, EndTick {
 	
 	public static final String MOD_ID = "mc_multimeter"; // is this needed? i saw other people do it in tutorials
 	
-	public static final String VERSION = "1.0.0"; // updated by updateVersion script with sed :)
+	public static final String VERSION = "1.0.1"; // updated by updateVersion script with sed :)
 	
 	public static boolean startJFrame() {
 		if (Utils.running()) {
